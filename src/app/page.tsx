14a1ebcd38de1ac011791f7378f54246ca5068d1
Muneeb Haq <muh5329@gmail.com>
2024-04-17 01:54:10 -0400

    added sections for webapp previews with animated gifs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,31 @@
 import Link from "next/link";
 
-
+import Image from 'next/image'
 const blocks = [
   {
-    "title": "title",
-    "image": "image/path",
-    "description": " a little site thats magic",
-    "url": "www.google.com"
+    "title": "Wave Collapse Editor",
+    "image": "/websites/WaveTransformCollapse.gif",
+    "description": " Wave Transform collapse editor",
+    "url": "https://main.d3rbvqutrpbvur.amplifyapp.com/",
+    "gh": "https://github.com/muh5329/WaveCollapseEditor"
+  }, {
+    "title": "Space Portfolio",
+    "image": "/websites/animatedSpacePortfolio.gif",
+    "description": "3D planet with a controllable spaceship",
+    "url": "https://main.d15nu8mrhnpxou.amplifyapp.com",
+    "gh": "https://github.com/muh5329/SpacePortfolio"
   }, {
     "title": "title",
-    "image": "image/path",
+    "image": "/icons/git-lnk.png",
     "description": " a little site thats magic",
-    "url": "www.google.com"
+    "url": "www.google.com",
+    "gh": "https://github.com/muh5329/WaveCollapseEditor"
   }, {
     "title": "title",
-    "image": "image/path",
+    "image": "/icons/git-lnk.png",
     "description": " a little site thats magic",
-    "url": "www.google.com"
-  }, {
-    "title": "title",
-    "image": "image/path",
-    "description": " a little site thats magic",
-    "url": "www.google.com"
+    "url": "www.google.com",
+    "gh": "https://github.com/muh5329/WaveCollapseEditor"
   },
 
 
@@ -33,11 +37,36 @@ export default function HomePage() {
       <div className="  grid gap-4 grid-cols-3 grid-rows-3">
         {blocks.map((block, index) => (
           <div key={index}>
-            <div key={index + "inner"}>{block.title}</div>
+            <a href={block.url}>
+              <Image
+                src={block.image}
+                width={500}
+                height={500}
+                alt="LinkedIn Logo"
+                key={index + "img"}
+              />
+            </a>
+
+            <div className="grid grid-cols-2 grid-rows-1" key={index + "inner"}>
+              <div className="text-lg font-bold"> {block.title}</div>
+              <a href={block.gh}>
+                <Image
+                  src="/icons/git-lnk.png"
+                  width={25}
+                  height={25}
+                  alt="LinkedIn Logo"
+                  key={index + "sourceIcon"}
+                />
+              </a>
+
+              <div className="text-gray-50 text-sm ">
+                {block.description}
+              </div>
+
+            </div>
           </div>
         ))}
       </div>
-      Hello (Profile Page in progress)
     </main>
   );
 }
