4efdbf0d39b95362337f425119d9fd71c8df61e8
Muneeb Haq <muh5329@gmail.com>
2024-05-06 02:27:16 -0400

    added key to derived blahblah

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -49,16 +49,17 @@ export default function ProjectsPage
   return (
     <main className=" h-dvh bg-yellow-500">
       <div className='flex items-center justify-center  gap-4 p-10'>
-        {tags.map((tag) =>
-          <div className='bg-gray-50 rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center '>
+        {tags.map((tag, i) =>
+          <div key={i} className='bg-gray-50 rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center '>
             {tag}
           </div>
         )}
 
       </div>
       <div className='grid grid-cols-4 justify-items-center gap-7 p-10'>
-        {projects.map((project) =>
-          <div className='rounded-lg col-span-1 row-span-1 bg-gray-50 w-[300px] h-[250px]'>
+        {projects.map((project, i) =>
+          <div key={i}
+            className='rounded-lg col-span-1 row-span-1 bg-gray-50 w-[300px] h-[250px]'>
             {project.name}
           </div>
         )}
