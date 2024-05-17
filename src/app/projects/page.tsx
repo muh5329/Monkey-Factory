7fbf5311fd1667f07dd4da5903bf01a64bc4d013
Muneeb Haq <muh5329@gmail.com>
2024-05-17 00:46:05 -0400

    filtering for projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,35 @@
+'use client'
 
 import Image from 'next/image'
-export default function ProjectsPage
-  () {
+import { useMemo, useState } from 'react';
+export default function ProjectsPage() {
 
-  const projects = [
+  interface Project {
+    name: string;
+    image: string;
+    link: string;
+    repo: string;
+    tags: string[];
+  }
+
+  const generateTopTags = (projects: Array<Project>): Array<string> => {
+    return [... new Set(projects.flatMap((m) => m.tags))];
+  }
+
+  function intersection<T>(arr1: T[], arr2: T[]): T[] {
+    return arr1.filter(value => arr2.includes(value));
+  }
+
+  const filterBySelectedTags = (projects: Array<Project>, selectedTags: string[]): Array<Project> => {
+    if (selectedTags.length > 0) {
+      return projects.filter((item) => intersection(item.tags, selectedTags).length > 0)
+    } else {
+      return projects;
+    }
+  }
+
+
+  const projects: Array<Project> = [
     {
       "name": "project1",
       "image": "/websites/mics.jpg",
@@ -16,25 +42,25 @@ export default function ProjectsPage
       "image": "/websites/mics.jpg",
       "link": "",
       "repo": "https://github.com/muh5329",
-      "tags": []
+      "tags": ["test"]
     }, {
       "name": "project1",
       "image": "/websites/mics.jpg",
       "link": "",
       "repo": "https://github.com/muh5329",
-      "tags": []
+      "tags": ["math"]
     }, {
       "name": "project1",
       "image": "/websites/mics.jpg",
       "link": "",
       "repo": "https://github.com/muh5329",
-      "tags": []
+      "tags": ["webgl", "design"]
     }, {
       "name": "project1",
       "image": "/websites/mics.jpg",
       "link": "",
       "repo": "https://github.com/muh5329",
-      "tags": []
+      "tags": ["random", "c"]
     }, {
       "name": "project1",
       "image": "/websites/mics.jpg",
@@ -42,23 +68,51 @@ export default function ProjectsPage
       "repo": "https://github.com/muh5329",
       "tags": []
     },
-
   ];
 
-  const tags = ["All", "threejs/Webgl", "design", "web", "tag"];
+
+
+  const tags: Array<string> = useMemo(
+    () => generateTopTags(projects),
+    [projects]
+  );
+
+  var [selectedTags, setSelectedTags] = useState<string[]>([]);
+  var [unselectedTags, setUnselectedTags] = useState<string[]>(tags);
+
+  var [projectList, setProjectList] = useState<Array<Project>>(projects);
+
+  const selectTag = (tag: string) => {
+    setSelectedTags([...selectedTags, tag]);
+    const updatedItems = unselectedTags.filter(item => item !== tag);
+    setUnselectedTags(updatedItems);
+    setProjectList(filterBySelectedTags(projects, [...selectedTags, tag]));
+  }
+
+  const unselectTag = (tag: string) => {
+    setUnselectedTags([...unselectedTags, tag]);
+    const updatedItems = selectedTags.filter(item => item !== tag);
+    setSelectedTags(updatedItems);
+    setProjectList(filterBySelectedTags(projects, updatedItems));
+  }
 
   return (
     <main className=" min-h-screen bg-yellow-500">
       <div className='flex items-center justify-center gap-4 p-10'>
-        {tags.map((tag, i) =>
-          <div key={i} className='bg-gradient-to-r from-slate-200 to-slate-50 rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center '>
+        {selectedTags.map((tag, i) =>
+          <div key={i} className="bg-slate-500  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center" onClick={() => unselectTag(tag)}>
+            {tag}
+          </div>
+        )}
+        {unselectedTags.map((tag, i) =>
+          <div key={i} className="bg-slate-200  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center" onClick={() => selectTag(tag)} >
             {tag}
           </div>
         )}
 
       </div>
       <div className='grid grid-cols-4 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4  min-[320px]:grid-cols-1 justify-items-center  p-10'>
-        {projects.map((project, i) =>
+        {projectList.map((project, i) =>
           <div key={i}
             className='rounded-lg flex flex-col  col-span-1 row-span-1 m-5 bg-gradient-to-r from-cyan-500 to-blue-500 w-[250px] h-[200px] '>
 
