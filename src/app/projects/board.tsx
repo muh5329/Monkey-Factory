78c90a068c02d372ce8387550d469d9bcdca2e99
Muneeb Haq <muh5329@gmail.com>
2024-05-20 23:56:09 -0400

    Move client interactive code into its own component , start work on moving project list into db

---
+++ src/app/projects/board.tsx
@@ -0,0 +1,137 @@
+'use client'
+
+import { Project } from "./page";
+import { Dispatch, SetStateAction, useState } from 'react';
+import Image from 'next/image'
+
+interface Board {
+    tags : string[]
+    projectList : Array<Project>
+    projects : Array<Project>
+}
+
+interface TagProps {
+  selectedTags : string[]
+  unselectedTags : string[]
+  setSelectedTags : Dispatch<SetStateAction<string[]>>
+  setUnselectedTags : Dispatch<SetStateAction<string[]>>
+  projectList : Array<Project>
+  setProjectList :  Dispatch<SetStateAction<Array<Project>>>
+}
+
+const Tags = (props :TagProps) =>{
+
+  function intersection<T>(arr1: T[], arr2: T[]): T[] {
+      return arr1.filter(value => arr2.includes(value));
+  }
+
+  const selectTag = (tag: string) => {
+      props.setSelectedTags([...props.selectedTags, tag]);
+      const updatedItems = props.unselectedTags.filter(item => item !== tag);
+      props.setUnselectedTags(updatedItems);
+      props.setProjectList(filterBySelectedTags( props.projectList, [...props.selectedTags, tag]));
+    }
+  
+  const unselectTag = (tag: string) => {
+      props.setUnselectedTags([...props.selectedTags, tag]);
+      const updatedItems = props.unselectedTags.filter(item => item !== tag);
+      props.setSelectedTags(updatedItems);
+      props.setProjectList(filterBySelectedTags( props.projectList, [...props.selectedTags, tag]));
+  }
+
+  const filterBySelectedTags = (projects: Array<Project>, selectedTags: string[]): Array<Project> => {
+  if (selectedTags.length > 0) {
+      return projects.filter((item) => intersection(item.tags, selectedTags).length > 0)
+  } else {
+      return projects;
+  }
+  }
+
+  return (<>
+      {props.selectedTags.length > 0 &&
+        <div className="bg-slate-200  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center">
+          All
+        </div>
+      }
+
+      {props.selectedTags.length <= 0 &&
+        <div className="bg-slate-500  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center" >
+          All
+        </div>
+      }
+
+
+      {props.selectedTags.map((tag, i) =>
+        <div key={i} className="bg-slate-500  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center" onClick={() => unselectTag(tag)}>
+          {tag}
+        </div>
+      )}
+      {props.unselectedTags.map((tag, i) =>
+        <div key={i} className="bg-slate-200  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center" onClick={() => selectTag(tag)} >
+          {tag}
+        </div>
+      )}
+
+  </>);
+}
+
+  
+export default function Board(props:Board) {
+    var [selectedTags,setSelectedTags]  = useState<string[]>([]);
+    var [unselectedTags, setUnselectedTags] = useState<string[]>(props.tags);
+    var [projectList,setProjectList] = useState(props.projectList);
+
+    return (<>
+        <div className='flex items-center justify-center gap-4 p-10'>
+          <Tags
+            selectedTags={selectedTags}
+            unselectedTags={unselectedTags}
+            setSelectedTags={setSelectedTags}
+            setUnselectedTags={setUnselectedTags}
+            projectList={projectList}
+            setProjectList={setProjectList}
+            
+          />
+        </div>
+
+        
+        <div className='grid grid-cols-4 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4  min-[320px]:grid-cols-1 justify-items-center  p-10'>
+          {projectList.map((project, i) =>
+            <div key={i}
+              className='rounded-lg flex flex-col  col-span-1 row-span-1 m-5 bg-gradient-to-r from-cyan-500 to-blue-500 w-[250px] h-[200px] '>
+
+              <div className='rounded-full w-[250px] h-[200px] shadow-2xl overflow-hidden relative inset-0  '>
+                <Image
+                  src={project.image}
+                  layout='fill'
+                  objectFit='cover'
+                  alt="Picture of the author"
+                />
+              </div>
+
+
+
+              <div className='relative '>
+
+                <div className=' text-slate-50 font-bold pl-3 bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-lg z-30 absolute bottom-0 left-0 w-[95%]' >
+                  {project.name}
+                </div>
+
+                <a target="_blank" rel="noopener noreferrer" href={project.repo}>
+                  <div className=' bg-gradient-to-r from-purple-500 to-pink-500 col-span-1 z-40 absolute bottom-0 right-0 rounded-lg' >
+                    <Image
+                      src="/icons/github-mark.png"
+                      width={25}
+                      height={25}
+                      alt=""
+                    />
+                  </div>
+                </a>
+
+              </div>
+
+            </div>
+          )}
+        </div>
+    </>);
+} 
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,48 +1,20 @@
-'use client'
+'use server'
 
 import Image from 'next/image'
-import { useMemo, useState , useEffect} from 'react';
+import Board from './board';
 
-export default function ProjectsPage() {
-
-
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('/api/trpc');
-      const result = await response.json();
-      setData(result.data);
-    };
-
-    fetchData();
-  }, []);
-
-
-  interface Project {
-    name: string;
-    image: string;
-    link: string;
-    repo: string;
-    tags: string[];
-  }
+export interface Project {
+  name: string;
+  image: string;
+  link: string;
+  repo: string;
+  tags: string[];
+}
+export default  async function ProjectsPage() {
 
   const generateTopTags = (projects: Array<Project>): Array<string> => {
     return [... new Set(projects.flatMap((m) => m.tags))];
   }
-
-  function intersection<T>(arr1: T[], arr2: T[]): T[] {
-    return arr1.filter(value => arr2.includes(value));
-  }
-
-  const filterBySelectedTags = (projects: Array<Project>, selectedTags: string[]): Array<Project> => {
-    if (selectedTags.length > 0) {
-      return projects.filter((item) => intersection(item.tags, selectedTags).length > 0)
-    } else {
-      return projects;
-    }
-  }
-
 
   const projects: Array<Project> = [
     {
@@ -87,101 +59,16 @@ export default function ProjectsPage() {
 
 
 
-  const tags: Array<string> = useMemo(
-    () => generateTopTags(projects),
-    [projects]
-  );
+  const tags: Array<string> = generateTopTags(projects);
 
-  var [selectedTags, setSelectedTags] = useState<string[]>([]);
-  var [unselectedTags, setUnselectedTags] = useState<string[]>(tags);
-
-  var [projectList, setProjectList] = useState<Array<Project>>(projects);
-
-  const selectTag = (tag: string) => {
-    setSelectedTags([...selectedTags, tag]);
-    const updatedItems = unselectedTags.filter(item => item !== tag);
-    setUnselectedTags(updatedItems);
-    setProjectList(filterBySelectedTags(projects, [...selectedTags, tag]));
-  }
-
-  const unselectTag = (tag: string) => {
-    setUnselectedTags([...unselectedTags, tag]);
-    const updatedItems = selectedTags.filter(item => item !== tag);
-    setSelectedTags(updatedItems);
-    setProjectList(filterBySelectedTags(projects, updatedItems));
-  }
-
-
+  var projectList : Array<Project> = projects;
 
   return (
     <main className=" min-h-screen bg-yellow-500">
-      <div className='flex items-center justify-center gap-4 p-10'>
-
-        {selectedTags.length > 0 &&
-          <div className="bg-slate-200  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center">
-            All
-          </div>
-        }
-
-        {selectedTags.length <= 0 &&
-          <div className="bg-slate-500  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center" >
-            All
-          </div>
-        }
-
-
-        {selectedTags.map((tag, i) =>
-          <div key={i} className="bg-slate-500  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center" onClick={() => unselectTag(tag)}>
-            {tag}
-          </div>
-        )}
-        {unselectedTags.map((tag, i) =>
-          <div key={i} className="bg-slate-200  rounded-lg pt-1 pb-1 pl-2 pr-2 h-fit text-center" onClick={() => selectTag(tag)} >
-            {tag}
-          </div>
-        )}
-
-      </div>
-      <div className='grid grid-cols-4 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-4  min-[320px]:grid-cols-1 justify-items-center  p-10'>
-        {projectList.map((project, i) =>
-          <div key={i}
-            className='rounded-lg flex flex-col  col-span-1 row-span-1 m-5 bg-gradient-to-r from-cyan-500 to-blue-500 w-[250px] h-[200px] '>
-
-            <div className='rounded-full w-[250px] h-[200px] shadow-2xl overflow-hidden relative inset-0  '>
-              <Image
-                src={project.image}
-                layout='fill'
-                objectFit='cover'
-                alt="Picture of the author"
-              />
-            </div>
-
-
-
-            <div className='relative '>
-
-              <div className=' text-slate-50 font-bold pl-3 bg-gradient-to-r from-violet-500 to-fuchsia-500 rounded-lg z-30 absolute bottom-0 left-0 w-[95%]' >
-                {project.name}
-              </div>
-
-              <a target="_blank" rel="noopener noreferrer" href={project.repo}>
-                <div className=' bg-gradient-to-r from-purple-500 to-pink-500 col-span-1 z-40 absolute bottom-0 right-0 rounded-lg' >
-                  <Image
-                    src="/icons/github-mark.png"
-                    width={25}
-                    height={25}
-                    alt=""
-                  />
-                </div>
-              </a>
-
-            </div>
-
-          </div>
-        )}
-      </div>
-
-
+      <Board
+        projectList={projectList}
+        tags={tags} 
+        projects={projects}      />
     </main>
   );
 }
