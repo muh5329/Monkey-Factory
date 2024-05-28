f0a23001e5c39965020844f7282af47753068907
Muneeb Haq <muh5329@gmail.com>
2024-05-28 11:33:44 -0400

    revert sqlite to use webclient, update version

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,4 +1,4 @@
-import { createClient, type Client } from "@libsql/client";
+import { createClient, type Client } from "@libsql/client/web";
 import { drizzle } from "drizzle-orm/libsql";
 
 import { env } from "~/env";
